508b931125c119c4574ae48fccbef9b9606d4bdd
Caroline Sih <sih.caroline@gmail.com>
2020-02-14 23:29:23 -0800

    signup page

--- src/pages/SignUpPage/index.js
+++ src/pages/SignUpPage/index.js
@@ -1,178 +1,72 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Formik, Form, useField } from "formik";
-import * as Yup from "yup";
-import styled from "@emotion/styled";
-import "./styles.css";
-import "./styles-custom.css";
+import React from 'react';
+import { Formik, useFormik, Field } from 'formik';
+import { Input, FormFeedback, Form, FormGroup, Label, Button } from "reactstrap";
 
-const MyTextInput = ({ label, ...props }) => {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input> and alse replace ErrorMessage entirely.
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
+import * as Yup from 'yup';
 
-const MyCheckbox = ({ children, ...props }) => {
-  const [field, meta] = useField({ ...props, type: "checkbox" });
-  return (
-    <>
-      <label className="checkbox">
-        <input {...field} {...props} type="checkbox" />
-        {children}
-      </label>
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
+const customInputForm = ({field, form: {touched, errors}, ...props}) => (
+  <div>
+      <Input
+          invalid={!!(touched[field.name] && errors[field.name])}
+          {...field}
+          {...props} />
+      {touched[field.name] && errors[field.name] && <FormFeedback>{errors[field.name]}</FormFeedback>}
+  </div>
+);
 
-// Styled components ....
-const StyledSelect = styled.select`
-  color: var(--blue);
-`;
-
-const StyledErrorMessage = styled.div`
-  font-size: 12px;
-  color: var(--red-600);
-  width: 400px;
-  margin-top: 0.25rem;
-  &:before {
-    content: "❌ ";
-    font-size: 10px;
-  }
-  @media (prefers-color-scheme: dark) {
-    color: var(--red-300);
-  }
-`;
-
-const StyledLabel = styled.label`
-  margin-top: 1rem;
-`;
-
-const MySelect = ({ label, ...props }) => {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input> and alse replace ErrorMessage entirely.
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <StyledLabel htmlFor={props.id || props.name}>{label}</StyledLabel>
-      <StyledSelect {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <StyledErrorMessage>{meta.error}</StyledErrorMessage>
-      ) : null}
-    </>
-  );
-};
-
-// And now we can use these
 const SignUpPage = () => {
-  return (
-    <>
-      <h1>Subscribe!</h1>
-      <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-          acceptedTerms: false, // added for our checkbox
-          jobType: "" // added for our select
-        }}
-        validationSchema={Yup.object({
-          firstName: Yup.string()
-            .max(15, "Must be 15 characters or less")
-            .required("Required"),
-          lastName: Yup.string()
-            .max(20, "Must be 20 characters or less")
-            .required("Required"),
-          email: Yup.string()
-            .email("Invalid email addresss`")
-            .required("Required"),
-          acceptedTerms: Yup.boolean()
-            .required("Required")
-            .oneOf([true], "You must accept the terms and conditions."),
-          jobType: Yup.string()
-            // specify the set of valid values for job type
-            // @see http://bit.ly/yup-mixed-oneOf
-            .oneOf(
-              ["designer", "development", "product", "other"],
-              "Invalid Job Type"
-            )
-            .required("Required")
-        })}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        <Form>
-          <MyTextInput
-            label="First Name"
-            name="firstName"
-            type="text"
-            placeholder="Jane"
-          />
-          <MyTextInput
-            label="Last Name"
-            name="lastName"
-            type="text"
-            placeholder="Doe"
-          />
-          <MyTextInput
-            label="Email Address"
-            name="email"
-            type="email"
-            placeholder="jane@formik.com"
-          />
-          <MySelect label="Job Type" name="jobType">
-            <option value="">Select a job type</option>
-            <option value="designer">Designer</option>
-            <option value="development">Developer</option>
-            <option value="product">Product Manager</option>
-            <option value="other">Other</option>
-          </MySelect>
-          <MyCheckbox name="acceptedTerms">
-            I accept the terms and conditions
-          </MyCheckbox>
 
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </>
+  var validationSchema =  Yup.object({
+    firstName: Yup.string()
+      .max(15, 'Must be 15 characters or less')
+      .required('Required'),
+    lastName: Yup.string()
+      .max(20, 'Must be 20 characters or less')
+      .required('Required'),
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Required'),
+  })
+  return (
+    <Formik
+                initialValues={{
+                    email: '',
+                    address: '',
+                    password: ''
+                }}
+                validationSchema={validationSchema}
+                onSubmit={values => {
+                    // same shape as initial values
+                    console.log(values);
+                }}>
+              <div className="d-flex">
+                <img src="../../aidnlogo.png" className="img-thumbnail w-25 text-center m-auto"></img>
+                <Form className="w-50 mx-auto mt-5">
+                    <FormGroup>
+                        <Label for="firstName">first name</Label>
+                        <Field name="firstName" type={'text'} component={customInputForm}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="lastName">last name</Label>
+                        <Field name="lastName" type={'text'} component={customInputForm}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleEmail">email</Label>
+                        <Field name="email" type={'email'} component={customInputForm}/>
+                    </FormGroup>
+                    {/*<FormGroup>
+                        <Label for="address">Address</Label>
+                        <Field name="address" type={'text'} component={customInputForm}/>
+                    </FormGroup>*/}
+                    <FormGroup>
+                        <Label for="examplePassword">password</Label>
+                        <Field name="password" type={'password'} component={customInputForm}/>
+                    </FormGroup>
+                    <Button color="info" >Submit</Button>
+                </Form>
+              </div>
+            </Formik>
   );
 };
-function App() {
-  return <SignUpPage />;
-}
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
-
-
-
-
-// import React from 'react';
-
-// class SignUpPage extends React.Component {
-
-//   render() {
-//     return (
-//       <div>
-//         this is where the user creates an account
-//       </div>
-//     );
-//   }
-// }
-
-// export default SignUpPage;
+export default SignUpPage;
