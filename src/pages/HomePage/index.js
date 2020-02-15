cf8a45c4304505ebe5af08f68b4b706c97bf7309
Subhankar Panda <s1panda@ucsd.edu>
2020-02-14 23:47:11 -0800

    attempting azure deploy

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './index.module.css';
+import './index.css'; // Import css modules stylesheet as styles
 
 function App() {
   return (
-    <div className="home">
-      Go Caroline!
+    <div className='container h-100 d-flex'>
+      <div className="row my-auto w-100">
+        <div className="col-md-6">
+          <img src='/aidnlogo.png' className="img-fluid"></img>
+        </div>
+        <div className="col-md-6 d-flex">
+          <div className="my-auto text-center w-100 home-text">
+            some catchy tagline here
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
