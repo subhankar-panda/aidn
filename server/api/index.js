cdd52ded77cd277c22dd2cb19103c2b1319d5942
edwjchen <edwjchen@gmail.com>
2020-02-15 04:19:23 -0800

    uploading for signup done

--- server/api/index.js
+++ server/api/index.js
@@ -46,58 +46,6 @@ var sha512 = function(password, salt){
     };
 };
 
-// imageUrl = 'https://dotesports-media.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2019/12/10165716/Reeves.jpg';
-
-// const params = {
-//   'returnFaceId': 'true',
-//   'returnFaceLandmarks': 'false',
-//   'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
-//       'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
-// };
-
-// const options = {
-//   uri: uriBase+"face/v1.0/detect",
-//   qs: params,
-//   body: '{"url": ' + '"' + imageUrl + '"}',
-//   headers: {
-//       'Content-Type': 'application/json',
-//       'Ocp-Apim-Subscription-Key' : subscriptionKey
-//   }
-// };
-
-// request.post(options, (error, response, body) => {
-//   if (error) {
-//     console.log('Error: ', error);
-//     return;
-//   }
-//   let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
-//   console.log('JSON Response\n');
-//   console.log(jsonResponse);
-
-//   const options2 = {
-//     uri: uriBase+'face/v1.0/persongroups/treehacks',
-//     body: JSON.stringify({
-//       "name": "group2",
-//       "userData": "user-provided data attached to the person group.",
-//       "recognitionModel": "recognition_02"
-//   }),
-//     qs: {
-//       'personGroupId':"treehacks"
-//     },
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Ocp-Apim-Subscription-Key' : subscriptionKey
-//     }
-//   };
-//   request.put(options2, (error, response, body2) => {
-//     if (error) {
-//       console.log('Error: ', error);
-//       return;
-//     }
-//     console.log(body2)
-//   })
-// });
-
 router.get('/', (req, res) => console.log('this is the /api/ handler'))
 
 router.post('/signup', upload.single('pic'), (req, res) => {
@@ -170,52 +118,6 @@ router.post('/signup', upload.single('pic'), (req, res) => {
             });
         });
     });
-    //     var meta = { contentData: "image/jpeg" };
-    //     const task = ref.child(name).put(req.file, meta);
-    //     task.then(snapshot => snapshot.ref.getDownloadURL())
-    //         .then(function(url) { 
-    //         //store all the user data to firestore:
-    //         let userref = db.collection('users').doc(personId);
-    //         userref.get().then(doc => {
-    //             if (!doc.exists) {
-    //                 var data = {
-    //                     name: req.body.name,
-    //                     email: req.body.email,
-    //                     salt: salt,
-    //                     hash: hash,
-    //                     picUrl: url, 
-    //                     personGroupId: "treehacks",
-    //                     personId: personId,
-    //                     role: "user"
-    //                 }
-    //                 db.collection('users').doc(personId).set(data).then(ref => {
-    //                     console.log('Added document with pid: ', personId);
-
-    //                     // create personGroup person
-    //                     let opts = {
-    //                         uri: uriBase+"face/v1.0/face/v1.0/persongroups/treehacks/persons/persistedFaces",
-    //                         body: JSON.stringify({
-    //                             "url":url
-    //                         }),
-    //                         headers: {
-    //                             'Content-Type': 'application/json',
-    //                             'Ocp-Apim-Subscription-Key' : subscriptionKey
-    //                         }
-    //                     };
-    //                     request.post(options, (error, response, body) => {
-    //                         if (error) {
-    //                             console.log('Error: ', error);
-    //                             return;
-    //                         }
-    //                         res.sendStatus(200);
-    //                     });
-    //                 });
-    //             } else {
-    //                 res.sendStatus(500);
-    //             }
-    //         });
-    //     });
-    // });
 });
 
 router.post('/detectFaces', (req, res) => {
