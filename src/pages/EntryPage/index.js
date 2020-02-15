c785c6460e0afc0b2e78026195e7cdaecdff697e
Caroline Sih <sih.caroline@gmail.com>
2020-02-15 15:03:57 -0800

    data in state

--- src/pages/EntryPage/index.js
+++ src/pages/EntryPage/index.js
@@ -1,181 +1,149 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { throwStatement } from '@babel/types';
 
 class EntryPage extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      arthritis: false,
+      cancer: false,
+      diabetes: false,
+      depression: false,
+      epilepsyseizures: false,
+      heartproblems: false,
+      heartsurgery: false,
+      highbp: false,
+      psycdisease: false,
+      stroke: false,
+      thyroid: false,
+    }
+  }
+ /*handleEmailChange: function(e) {
+   this.setState({email: e.target.value});
+},
+handlePasswordChange = function(e) {
+   this.setState({password: e.target.value});
+}*/
+  handleCheckbox = (label, e) => {
+    this.setState({[label]: e.target.checked})
+  }
+  handleAllergies = (e) => {
+    this.setState({allergies: e.target.value})
+  }
+  handleConditions = (e) => {
+    this.setState({conditions: e.target.value})
+  }
+  handleMessage = (e) => {
+    this.setState({message: e.target.value})
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="w-75 mx-auto">
         <Form>
-          <h4 className="mt-4">past medical history: please check all that apply to you:</h4>
+        <h3 className="mt-4 mx-auto">past medical history</h3>
+        <h4 className="mt-4">please check all that apply to you:</h4>
+          
           <FormGroup check>
             <Label check>
-              <Input type="checkbox" />{' '}
+              <Input type="checkbox" onClick={(e) => this.handleCheckbox('arthritis', e)} value={this.state.arthritis}/>{' '}
               Arthritis
             </Label>
         </FormGroup>
         <FormGroup check>
             <Label check>
-              <Input type="checkbox" />{' '}
+              <Input type="checkbox" onClick={(e) => this.handleCheckbox('cancer', e)} value={this.state.cancer}/>{' '}
               Cancer
             </Label>
         </FormGroup>
         <FormGroup check>
             <Label check>
-              <Input type="checkbox" />{' '}
+              <Input type="checkbox" onClick={(e) => this.handleCheckbox('depression', e)} value={this.state.depression}/>{' '}
               Depression
             </Label>
         </FormGroup>
         <FormGroup check>
             <Label check>
-              <Input type="checkbox" />{' '}
+              <Input type="checkbox" onClick={(e) => this.handleCheckbox('diabetes', e)} value={this.state.diabetes}/>{' '}
               Diabetes
             </Label>
         </FormGroup>
         <FormGroup check>
             <Label check>
-              <Input type="checkbox" />{' '}
+              <Input type="checkbox" onClick={(e) => this.handleCheckbox('epilepsy/seizures', e)} value={this.state.epilepsyseizures}/>{' '}
               Epilepsy/Seizures
             </Label>
         </FormGroup>
         <FormGroup check>
             <Label check>
-              <Input type="checkbox" />{' '}
+              <Input type="checkbox" onClick={(e) => this.handleCheckbox('heartproblems', e)} value={this.state.heartproblems}/>{' '}
               Heart Problems
             </Label>
         </FormGroup>
         <FormGroup check>
             <Label check>
-              <Input type="checkbox" />{' '}
+              <Input type="checkbox" onClick={(e) => this.handleCheckbox('heartsurgery', e)} value={this.state.heartsurgery}/>{' '}
               Heart Surgery
             </Label>
         </FormGroup>
         <FormGroup check>
             <Label check>
-              <Input type="checkbox" />{' '}
+              <Input type="checkbox" onClick={(e) => this.handleCheckbox('highbp', e)} value={this.state.highbp}/>{' '}
               High Blood Pressure
             </Label>
         </FormGroup>
         <FormGroup check>
             <Label check>
-              <Input type="checkbox" />{' '}
+              <Input type="checkbox" onClick={(e) => this.handleCheckbox('psyc', e)} value={this.state.psycdisease}/>{' '}
               Psychiatric Disease
             </Label>
         </FormGroup>
         <FormGroup check>
             <Label check>
-              <Input type="checkbox" />{' '}
+              <Input type="checkbox" onClick={(e) => this.handleCheckbox('stroke', e)} value={this.state.stroke}/>{' '}
               Stroke
             </Label>
         </FormGroup>
         <FormGroup check>
             <Label check>
-              <Input type="checkbox" />{' '}
+              <Input type="checkbox" onClick={(e) => this.handleCheckbox('thyroid', e)} value={this.state.thyroid}/>{' '}
               Thyroid
             </Label>
         </FormGroup>
 
         <FormGroup style={{marginTop: '15px'}}>
         <Label for="exampleText">please enter any serious allergies, separated by commas and spaces (ex. "peanuts, ibuprofen"):</Label>
-        <Input type="textarea" name="text" id="exampleText" />
+        <Input type="textarea" name="text" onChange={(e) => this.handleAllergies(e)} value={this.state.allergies} />
         </FormGroup>
 
         <FormGroup style={{marginTop: '15px'}}>
-        <Label for="exampleText">please enter any alternate conditions helpers should be aware of:</Label>
-        <Input type="textarea" name="text" id="exampleText" />
+        <Label for="exampleText">please enter any alternate conditions your aid network should be aware of:</Label>
+        <Input type="textarea" name="text" onChange={(e) => this.handleConditions(e)} value={this.state.conditions} />
         </FormGroup>
 
         <FormGroup style={{marginTop: '15px'}}>
-        <Label for="exampleText">in the event of an emergency, we will display this message on your screen:</Label>
+        <Label for="exampleText">customize your emergency screen message:</Label>
         <Label for="exampleText">(ex. "I have had a heart transplant. Please do not administer a defibrillator.")</Label>
-        <Input type="textarea" name="text" id="exampleText" />
+        <Input type="textarea" name="text" onChange={(e) => this.handleMessage(e)} value={this.state.message} />
         </FormGroup>
       
-      <Button>Submit</Button>
+      <Button onClick={this.handleSubmit}>Submit</Button>
     </Form>
+  
       </div>
+
     );
+    
+  }
+  handleSubmit = () => {
+    console.log("diabetes: " + this.state.diabetes);
+    console.log("allergies: " + this.state.allergies);
+    console.log("conditions: " + this.state.conditions);
+    console.log("message: " + this.state.message);
   }
 }
 
 export default EntryPage;
-
-{/*
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-
-const Example = (props) => {
-  return (
-    <Form>
-      <FormGroup>
-        <Label for="exampleEmail">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleSelect">Select</Label>
-        <Input type="select" name="select" id="exampleSelect">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </Input>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleSelectMulti">Select Multiple</Label>
-        <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </Input>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleText">Text Area</Label>
-        <Input type="textarea" name="text" id="exampleText" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleFile">File</Label>
-        <Input type="file" name="file" id="exampleFile" />
-        <FormText color="muted">
-          This is some placeholder block-level help text for the above input.
-          It's a bit lighter and easily wraps to a new line.
-        </FormText>
-      </FormGroup>
-      <FormGroup tag="fieldset">
-        <legend>Radio Buttons</legend>
-        <FormGroup check>
-          <Label check>
-            <Input type="radio" name="radio1" />{' '}
-            Option one is this and that—be sure to include why it's great
-          </Label>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="radio" name="radio1" />{' '}
-            Option two can be something else and selecting it will deselect option one
-          </Label>
-        </FormGroup>
-        <FormGroup check disabled>
-          <Label check>
-            <Input type="radio" name="radio1" disabled />{' '}
-            Option three is disabled
-          </Label>
-        </FormGroup>
-      </FormGroup>
-      <FormGroup check>
-        <Label check>
-          <Input type="checkbox" />{' '}
-          Check me out
-        </Label>
-      </FormGroup>
-      <Button>Submit</Button>
-    </Form>
-  );
-}
-
-export default Example; */}
