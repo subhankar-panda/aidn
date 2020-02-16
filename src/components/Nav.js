b3cf9e1d1d9c0afa1385747e7d11959f9b2b780b
Subhankar Panda <s1panda@ucsd.edu>
2020-02-16 01:10:08 -0800

    added dash

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -22,32 +22,32 @@ const TopNav = (props) => {
   const {authenticated} = props;
   return (
     <div>
-      <Navbar color="light" light expand="md">
+      <Navbar color="light" light expand="md" className="bg-transparent">
         <NavbarBrand href="/">aidn.</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              {!authenticated && <NavLink href="/signup/"><Button outline color='secondary'>sign up</Button></NavLink>}
+              {!authenticated && <NavLink href="/signup/"><Button outline color='secondary'>Sign Up</Button></NavLink>}
             </NavItem>
             <NavItem>
-              {!authenticated && <NavLink href="/login/"><Button outline color='secondary'>log in</Button></NavLink>}
+              {!authenticated && <NavLink href="/login/"><Button outline color='secondary'>Log In</Button></NavLink>}
             </NavItem>
             <NavItem>
-              <NavLink href="/assist/"><Button outline color='secondary'>assistant</Button></NavLink>
+              <NavLink href="/assist/"><Button outline color='secondary'>Assistant</Button></NavLink>
             </NavItem>
             <NavItem>
-              {authenticated && <NavLink href="/medical-history/"><Button outline color='secondary'>medical history</Button></NavLink>}
+              {authenticated && <NavLink href="/medical-history/"><Button outline color='secondary'>Medical History</Button></NavLink>}
             </NavItem>
             <NavItem>
-              {authenticated && <NavLink href="/doctors/"><Button outline color='secondary'>doctors</Button></NavLink>}
+              {authenticated && <NavLink href="/doctors/"><Button outline color='secondary'>Doctors</Button></NavLink>}
             </NavItem>
             <NavItem>
               <NavLink href="/emergency"><Button color='danger'>Medical Emergency</Button></NavLink>
             </NavItem>
           </Nav>
-          {props.user && <NavbarText className='pr-2'>hi, {props.user.name} </NavbarText>}
-          <NavbarText><img className="rounded-circle img-responsive" style={{width: '3rem'}} src={props.user && props.user.picUrl}></img></NavbarText>
+          {props.user && <NavbarText className='pr-2'>{props.user.name} </NavbarText>}
+          <NavbarText><img className="rounded-circle img-responsive" style={{width: '3rem', height: '3rem'}} src={props.user && props.user.picUrl}></img></NavbarText>
         </Collapse>
       </Navbar>
     </div>
